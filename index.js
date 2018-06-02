21a4fb4dcd6863be8ecb7ec0deb2eafe448a3fc6
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 21:58:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mus = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
 
-const inst = []
+const inst = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
 
 function theBeatlesPlay(array1, array2){
