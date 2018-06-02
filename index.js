1516e16e9be09c05768d0149899344553a297f91
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 22:02:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function BeatlesLoops() {
-  const mus = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
 
-const inst = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
-}
-
-function theBeatlesPlay(array1, array2){
-
+function theBeatlesPlay(musicians, instruments){
+var a = [];
 }
 
 function johnLennonFacts() {
