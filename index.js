c023d135ac8090ef5e6e42d2100f49bbf854b0ff
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 22:01:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const mus = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+function Beatles Loops() {
+  const mus = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
 
 const inst = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
-
+}
 
 function theBeatlesPlay(array1, array2){
 
+}
+
+function johnLennonFacts() {
+  
+}
+
+function iLoveTheBeatles() {
+  
 }
