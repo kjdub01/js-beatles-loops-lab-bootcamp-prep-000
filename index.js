9b37786636f74a0e58fa9b8f12d0ba9fb3cc95ab
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 22:09:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function theBeatlesPlay(musicians, instruments){
 var a = [];
 
 for (var i =0; 1 > musicians; ++i);
-Array.push(musicians[i]) + "plays" + instruments
+Array.push(musicians[i]) + "plays" + instruments[i])
+
 }
 
 function johnLennonFacts() {
